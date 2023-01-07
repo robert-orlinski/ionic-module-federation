25eb9f5898998e9398ad77323e79c203a7261aba
Robert Orliński <kontakt@robertorlinski.pl>
2023-01-07 20:51:06 +0100

    Setup everything with CRACO webpack.configure

---
+++ remote/craco.config.js
@@ -0,0 +1,47 @@
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const deps = require('./package.json').dependencies;
+
+module.exports = {
+	webpack: {
+		configure: (webpackConfig, { env, paths }) => {
+			webpackConfig.output.publicPath = 'auto';
+
+			const htmlWebpackPlugin = webpackConfig.plugins.find(
+				(plugin) => plugin.constructor.name === 'HtmlWebpackPlugin'
+			);
+
+			htmlWebpackPlugin.userOptions = {
+				...htmlWebpackPlugin.userOptions,
+				publicPath: paths.publicUrlOrPath,
+				excludeChunks: ['remote'],
+			};
+
+			webpackConfig.plugins = [
+				...webpackConfig.plugins,
+				new ModuleFederationPlugin({
+					name: 'remote',
+					remotes: {},
+					exposes: {
+						'./App': './src/App',
+					},
+					filename: 'remoteEntry.js',
+					shared: {
+						...deps,
+						react: {
+							singleton: true,
+							eager: true,
+							requiredVersion: deps['react'],
+						},
+						'react-dom': {
+							singleton: true,
+							eager: true,
+							requiredVersion: deps['react-dom'],
+						},
+					},
+				}),
+			];
+
+			return webpackConfig;
+		},
+	},
+};
